db1c2c06f9e9c0b844057fbb8d7d75759e3e80d3
Bogdan <bogdan7morozyuk@gmail.com>
2019-08-07 17:03:36 +0300

    Fetch and display users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,51 @@
-const App = () => {
+import { Component } from 'inferno';
+import UserService from './services/user.service';
+import Loader from './components/shared/Loader';
+import withLoading from './hocs/withLoading';
+import UserTable from './components/table/UserTable';
+import styles from './App.module.css';
 
-  return null;
-};
+const UserTableWithLoadingIndicator = withLoading(
+  UserTable,
+  Loader
+);
 
-export default App;
+export default class App extends Component {
+  state = {
+    data: [],
+    isLoading: false,
+  };
+
+  fetchUsers = () => {
+    this.setState({ isLoading: true }, () => {
+      UserService
+        .getAll()
+        .then(response => response.json())
+        .then(data => {
+          this.setState({
+            data,
+            isLoading: false
+          });
+        });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+
+    return (
+      <div className={styles.app}>
+        <UserTableWithLoadingIndicator
+          {...{
+            users: data,
+            isLoading,
+          }}
+        />
+      </div>
+    );
+  }
+}
